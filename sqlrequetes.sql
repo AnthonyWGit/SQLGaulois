b7bbe055f11aa7b4273b0c632e09d709aae5e0a9
Anthony Wetzstein <anthony.wetzstein@gmail.com>
2023-04-23 16:07:07 +0200

    More requests, database comming

--- sqlrequetes.sql
+++ sqlrequetes.sql
@@ -98,4 +98,48 @@ WHERE autoriser_boire.id_potion != 1
 
 __________________________________
 
+16
+INSERT INTO personnage (personnage.id_personnage, personnage.nom_personnage, personnage.adresse_personnage, personnage.id_lieu, personnage.id_specialite)
+VALUES
+	(46, "Champdeblix", "Ferme Hantassion", 6, 12)
 
+
+----------
+17
+INSERT INTO autoriser_boire (id_potion, id_personnage)
+VALUES
+	(1, 12)
+
+-----------
+18
+DELETE FROM casque
+WHERE id_casque = ANY ( SELECT * FROM (
+SELECT casque.id_casque  FROM casque LEFT JOIN prendre_casque ON casque.id_casque = prendre_casque.id_casque
+INNER JOIN type_casque ON casque.id_type_casque = type_casque.id_type_casque
+WHERE type_casque.nom_type_casque = "Grec" AND prendre_casque.qte IS NULL) c )
+
+------
+19
+UPDATE personnage
+SET id_lieu = (SELECT * FROM (SELECT id_lieu FROM lieu WHERE nom_lieu = "Condate") c ), adresse_personnage = "EN taule"
+WHERE id_personnage = (SELECT * FROM (SELECT id_personnage FROM personnage WHERE nom_personnage = "Zérozérosix") c )
+
+----------
+
+
+SELECT potion.id_potion, ingredient.id_ingredient FROM composer INNER JOIN potion ON composer.id_potion = potion.id_potion
+INNER JOIN ingredient ON composer.id_ingredient = ingredient.id_ingredient
+WHERE ingredient.nom_ingredient = "Persil" AND potion.nom_potion = "Soupe"
+
+
+DELETE FROM composer
+WHERE composer.id_potion = (SELECT * FROM (
+SELECT potion.id_potion FROM composer INNER JOIN potion ON composer.id_potion = potion.id_potion
+INNER JOIN ingredient ON composer.id_ingredient = ingredient.id_ingredient
+WHERE ingredient.nom_ingredient = "Persil" AND potion.nom_potion = "Soupe") c )
+20
+DELETE FROM composer
+WHERE id_ingredient IN (SELECT * FROM (
+SELECT ingredient.id_ingredient  FROM composer INNER JOIN potion ON composer.id_potion = potion.id_potion
+INNER JOIN ingredient ON composer.id_ingredient = ingredient.id_ingredient
+WHERE ingredient.nom_ingredient = "Persil" AND potion.nom_potion = "Soupe" ) c ) 
